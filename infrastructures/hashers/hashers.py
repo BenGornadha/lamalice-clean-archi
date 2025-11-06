8833ec28c6ba1c3d69482d1623a34db37fc206b3
BenGornad <benjamin.gornadha@gmail.com>
2025-11-06 21:01:18 +0100

    add second PasswordHasher

--- infrastructures/hashers/hashers.py
+++ infrastructures/hashers/hashers.py
@@ -1,4 +1,5 @@
 import bcrypt
+import hashlib
 from interfaces.password_hasher import PasswordHasherInterface
 
 
@@ -10,3 +11,12 @@ class BcryptPasswordHasher(PasswordHasherInterface):
         pw = password.encode('utf-8')
         salt = bcrypt.gensalt(rounds=self._rounds)
         return bcrypt.hashpw(pw, salt).decode('utf-8')
+
+
+class SHA256PasswordHasher(PasswordHasherInterface):
+    def __init__(self, salt: str = "default_salt"):
+        self._salt = salt
+
+    def hash(self, password: str) -> str:
+        salted_password = password + self._salt
+        return hashlib.sha256(salted_password.encode('utf-8')).hexdigest()
